c66a2a0ad99b0ee9c1907d30db093f85ed32a5da
caoke <kcao@libnet.sh.cn>
2016-08-30 14:42:32 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    licenses = f.read()
+
+install_requires = [
+    'python-ldap',
+    'flask-login'
+]
+
+setup(
+    name='flask-ldap-login',
+    version='0.0.1',
+    description='基于LDAP的用户认证 Flask Blueprint',
+    long_description=readme,
+    author='CaoKe',
+    author_email='hitakaken@gmail.com',
+    url='https://github.com/hitakaken/flask-ldap-login',
+    license=licenses,
+    platforms=["any"],
+    packages=['ldap_login'],
+    test_suite="test.tests",
+    install_requires=install_requires,
+    tests_require=['nose'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    # data_files=[('', ['README'])]
+)
