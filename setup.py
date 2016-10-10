1e7d46d3a9c34307925beee3d6c06da6afea42ef
caoke <kcao@libnet.sh.cn>
2016-10-10 11:49:17 +0800

    Initial Commit

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
 
 setup(
     name='flask-ldap-rbac',
-    version='0.0.1',
+    version='0.0.2',
     description='基于LDAP的用户认证 Flask Blueprint',
     long_description=readme,
     author='CaoKe',
